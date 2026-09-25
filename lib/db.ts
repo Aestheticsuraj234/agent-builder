ba7359e2bd23a03c19d62f58e0388107698fb4a0
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-09-25 11:18:15 +0530

    prisma 7 init

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,15 @@
-import 'dotenv/config';
-import postgres from '@prisma/orm-postgres/runtime';
-import type { Contract } from '@/prisma/contract.d';
-import contractJson from '@/prisma/contract.json' with { type: 'json' };
-
-export const db = postgres<Contract>({
-  contractJson,
-  url: process.env['DATABASE_URL']!,
-});
+import { PrismaClient } from "@/lib/generated/prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL, 
+}); 
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export default prisma; 
